aed26de089c7624321eae9b4ab80792f4266b314
Nikita Kiselev <kiselev2008@gmail.com>
2016-04-06 15:56:07 +0300

    steps

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,22 @@
-/**
- * Created by nikita on 04.04.16.
- */
+;(function($) {
+
+    var $clients = $("#clients"),
+        $spClientTitle = $("#sp-client-title"),
+        $spClientUrl = $('#sp-client-url');
+
+    $clients.on('click', '.client', function(event) {
+        var $this = $(this),
+            title = $this.data('title'),
+            url = $this.data('url');
+
+        $this.siblings().removeClass('selected');
+        $this.addClass('selected');
+        $spClientTitle.html(title);
+        $spClientUrl.html(
+            '<a href="' + url + '">' + url + '</a>'
+        );
+
+        event.preventDefault();
+    });
+
+})(jQuery);
